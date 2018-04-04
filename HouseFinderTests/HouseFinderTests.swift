8a2292e268ed1c30503c8c98d71d3804678dd2f9
Pablo Ortiz Rodríguez <pabloortizrodriguez1995@gmail.com>
2018-04-04 20:06:21 +0200

    Initial Commit

---
+++ HouseFinderTests/HouseFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  HouseFinderTests.swift
+//  HouseFinderTests
+//
+//  Created by Pablo Ortiz Rodríguez on 04/04/2018.
+//  Copyright © 2018 Pablo Ortiz Rodríguez. All rights reserved.
+//
+
+import XCTest
+@testable import HouseFinder
+
+class HouseFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
