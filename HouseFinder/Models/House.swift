900a11576a27182723da7d3ebbcfc69f739b50d4
Pablo Ortiz <pabloortizrodriguez1995@gmail.com>
2018-04-06 20:09:15 +0200

    20180406-001
    Adding House model to the project

---
+++ HouseFinder/Models/House.swift
@@ -0,0 +1,34 @@
+//
+//  House.swift
+//  HouseFinder
+//
+//  Created by Pablo Ortiz Rodríguez on 06/04/2018.
+//  Copyright © 2018 Pablo Ortiz Rodríguez. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class House {
+    
+    //MARK: Properties
+    
+    var name: String
+    var roomsNumber: Int
+    var bathsNumber: Int
+    var rating: Int
+    var latitude: CLLocationDegrees
+    var longitude: CLLocationDegrees
+    
+    //MARK: Initialization
+    
+    init(name: String, roomsNumber: Int, bathsNumber: Int, rating: Int, latitude: CLLocationDegrees, longitude: CLLocationDegrees){
+        self.name = name
+        self.roomsNumber = roomsNumber
+        self.bathsNumber = bathsNumber
+        self.rating = rating
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+}
