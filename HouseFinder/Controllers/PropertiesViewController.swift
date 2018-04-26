18e73f63fec499e9e0a632f571a3b08932518e23
Pablo Ortiz <pabloortizrodriguez1995@gmail.com>
2018-04-27 00:53:39 +0200

    20180427-001
    Starting the development of the property view

--- HouseFinder/Controllers/PropertiesViewController.swift
+++ HouseFinder/Controllers/PropertiesViewController.swift
@@ -8,15 +8,59 @@
 
 import UIKit
 
-class PropertiesViewController: UIViewController {
+class PropertiesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var modifyButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var actionsView: UIView!
+    @IBOutlet weak var propertiesTableView: UITableView!
+    var properties: [String] = ["a", "b", "c"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //TableView
+        self.propertiesTableView.delegate = self
+        self.propertiesTableView.dataSource = self
+        
+        actionsView.isHidden = true
         navigationItem.title = "Properties"
+        calculateButton.layer.cornerRadius = 20
+        modifyButton.layer.cornerRadius = 20
+        deleteButton.layer.cornerRadius = 20
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return properties.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = propertiesTableView.dequeueReusableCell(withIdentifier: "propertyCell", for: indexPath)
+        cell.textLabel?.text = properties[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        actionsView.isHidden = false
+    }
 
+    @IBAction func modifyProperty(_ sender: UIButton) {
+        actionsView.isHidden = true
+    }
+    
+    @IBAction func deleteProperty(_ sender: UIButton) {
+        actionsView.isHidden = true
+    }
+    
 }
